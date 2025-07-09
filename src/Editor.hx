adc54adc6a9689405ae3de210cab7e8bfb713dc6
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-09 15:36:04 -0800

    Added studio sunaba project

---
+++ src/Editor.hx
@@ -0,0 +1,30 @@
+package;
+
+import sunaba.ui.Widget;
+import sunaba.desktop.AcceptDialog;
+import sunaba.core.Vector2i;
+import sunaba.core.Vector2;
+import sunaba.core.Rect2i;
+import sunaba.desktop.PopupMenu;
+import sunaba.desktop.FileDialog;
+import sunaba.core.ArrayList;
+import sunaba.core.Variant;
+import sunaba.FileDialogMode;
+import sunaba.core.SubViewport;
+import sunaba.Runtime;
+import sunaba.App;
+import sunaba.core.PlatformService;
+import sunaba.core.DeviceType;
+import sunaba.input.InputEvent;
+import sunaba.input.InputService;
+import sunaba.Key;
+import sunaba.ui.Control;
+import sunaba.WindowMode;
+import sunaba.core.Element;
+import sunaba.desktop.Window;
+
+class Editor extends Widget {
+    override function init() {
+        load("app://Editor.suml");
+    }
+}
