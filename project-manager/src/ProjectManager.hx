5772a2f44d2dd8c9d26937dad8663aab1685c0a1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-09 17:56:19 -0800

    Updated ProjectManager

--- project-manager/src/ProjectManager.hx
+++ project-manager/src/ProjectManager.hx
@@ -22,9 +22,36 @@ import sunaba.ui.Control;
 import sunaba.WindowMode;
 import sunaba.core.Element;
 import sunaba.desktop.Window;
+import sunaba.ui.Button;
 
 class ProjectManager extends Widget {
     override function init() {
         load("app://ProjectManager.suml");
+
+        var openProjectButton = Button.toButton(rootElement.find("vbox/toolbar/hbox/openProjectButton"));
+        openProjectButton.pressed.connect((args: ArrayList) -> {
+            var openProjectDialog = new FileDialog();
+            openProjectDialog.fileMode = FileDialogMode.openFile;
+            openProjectDialog.useNativeDialog = true;
+            openProjectDialog.access = 2;
+            openProjectDialog.title = "Open Project";
+            openProjectDialog.addFilter("*.snbproj", "Sunaba Project");
+            rootElement.addChild(openProjectDialog);
+
+            openProjectDialog.fileSelected.connect((args: ArrayList) -> {
+                var path = args.get(0).toString();
+                openProjectDialog.hide();
+                openProjectDialog.delete();
+                openProject(path);
+            });
+
+            openProjectDialog.popupCentered(new Vector2i(0, 0));
+
+        });
+    }
+
+    function openProject(path: String) {
+        var programPath = Sys.programPath();
+        trace(programPath);
     }
 }
